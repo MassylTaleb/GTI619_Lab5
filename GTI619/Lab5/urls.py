fa80f43c13ca8e0fa0339cf155815a3ad9ca9023
Massyl Taleb <massyltaleb@gmail.co>
2020-03-28 21:02:40 -0400

    IM A MOTHER FKING BAWWS

--- GTI619/Lab5/urls.py
+++ GTI619/Lab5/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LogoutView
 from . import views
 
-
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('', views.signup, name='signup'),
-] 
+    path('', views.home, name='home'),
+    path('login/', LoginView.as_view(template_name = 'login.html'), name='login'),
+    path('logout/', LogoutView.as_view(next_page = 'login'), name='logout'),
+    path('signup/', views.signup, name='signup'),
+]
