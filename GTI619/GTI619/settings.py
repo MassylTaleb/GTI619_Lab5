4154908b33582fce77ebb9b9a6fd3d725177e7c7
kyo <kyo.kaizuka@gmail.com>
2020-04-01 02:43:37 -0400

    send email for real

--- GTI619/GTI619/settings.py
+++ GTI619/GTI619/settings.py
@@ -40,12 +40,12 @@ INSTALLED_APPS = [
     'rest_framework',
 ]
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = ''
-EMAIL_HOST_PASSWORD = ''
+EMAIL_HOST_USER = 'gti619eq09@gmail.com'
+EMAIL_HOST_PASSWORD = 'gti619newteam'
 AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
 ]
