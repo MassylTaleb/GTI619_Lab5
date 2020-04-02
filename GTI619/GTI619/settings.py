4daf427c10cfac30ca69ee63f6e41f56b9f4d924
jonyjmzETS <jonathan-caleb.jimenez-cruz.1@ens.etsmtl.ca>
2020-04-02 18:42:44 -0400

    premier jet

--- GTI619/GTI619/settings.py
+++ GTI619/GTI619/settings.py
@@ -34,10 +34,7 @@ INSTALLED_APPS = [
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'frontend',
-    'rest_framework'
+    'django.contrib.messages'
 ]
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
